4a843ea85807bed737404ad9dc1dc34682ae55a0
Alexia Farias <avfa24@gmail.com>
2025-04-24 15:44:31 -0300

    Ineteracao com usuarios

--- Main.java
+++ Main.java
@@ -9,6 +9,11 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         Combate combate = new Combate(jogador, inimigo);
+        System.out.println("O planeta está em chamas. O herói Water é a última esperança contra o avanço do império do fogo.");
+        System.out.println("De repente, surge um inimigo flamejante... Prepare-se para lutar!");
+
         combate.iniciarBatalha(scanner);
+
+        System.out.println(" A batalha acabou. A natureza agradece sua coragem!");
     }
 }
